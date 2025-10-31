e7672078ba24fb8216aae9126e3334264e312aad
Aleowyne <aleowyne@gmail.com>
2025-10-31 19:58:34 +0100

    ♻️ refactor: update tailwind v4 and others dependencies

---
+++ inertia/components/Footer.vue
@@ -0,0 +1,3 @@
+<template>
+  <footer class="text-center text-sm py-5">Copyright © YYYY Créateur - Projet</footer>
+</template>
---
+++ inertia/components/form/Error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-4 text-xs mb-5 text-red-600 dark:text-red-500">
+    <p v-if="props.error">
+      {{ props.error }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    error?: string
+  }>()
+</script>
